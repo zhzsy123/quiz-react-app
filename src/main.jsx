feaa81a5ddbe6aa47fe477fade3fb04ea2d25726
zhzsy123 <zhzsy123@gmail.com>
2026-04-05 23:50:15 -0700

    Fix blank page by switching to HashRouter and loading system styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
 import AppRouter from './router/AppRouter'
 import './index.css'
+import './system.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <AppProvider>
         <AppRouter />
       </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
