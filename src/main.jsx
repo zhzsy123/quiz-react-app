854b261be0bee84b2b270442398ba480a1f45f4c
zhzsy123 <zhzsy123@gmail.com>
2026-04-06 00:34:39 -0700

    Update main entry for phase two pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
-import AppRouter from './router/AppRouter'
+import AppRouterV2 from './router/AppRouterV2'
 import './index.css'
 import './system.css'
+import './system2.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <AppProvider>
-        <AppRouter />
+        <AppRouterV2 />
       </AppProvider>
     </HashRouter>
   </React.StrictMode>
