6e8af019806e5ec1f74a4cc097aeb5a6a2d699ec
zhzsy123 <zhzsy123@gmail.com>
2026-04-06 01:35:45 -0700

    Rework dashboard into split-view with left module rail

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
-import AppRouterV2 from './router/AppRouterV2'
+import AppRouterV3 from './router/AppRouterV3'
 import './index.css'
 import './system.css'
 import './system2.css'
+import './system3.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <AppProvider>
-        <AppRouterV2 />
+        <AppRouterV3 />
       </AppProvider>
     </HashRouter>
   </React.StrictMode>
