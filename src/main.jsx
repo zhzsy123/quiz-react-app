25b200a50991f8d1fcd2e27b9d6dba1e48056417
zhzsy123 <zhzsy123@gmail.com>
2026-04-06 02:47:34 -0700

    Enable wrong book and editable history with saved answers

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
-import AppRouterV3 from './router/AppRouterV3'
+import AppRouterV4 from './router/AppRouterV4'
 import './index.css'
 import './system.css'
 import './system2.css'
 import './system3.css'
+import './system4.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <AppProvider>
-        <AppRouterV3 />
+        <AppRouterV4 />
       </AppProvider>
     </HashRouter>
   </React.StrictMode>
